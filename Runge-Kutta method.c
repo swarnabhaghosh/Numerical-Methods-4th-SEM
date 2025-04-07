d98a93f81e02c1c931f0b40100dfccc1c0df21e5
Swarnabha Ghosh <swarnabha983@gmail.com>
2025-04-07 12:02:03 +0530

    commit

---
+++ Runge-Kutta method.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+float f(float x, float y)
+{
+        return (3*x+y*y);
+}
+void main()
+{
+        float x0, y0, h, xn, k1, k2, k3, k4, k;
+        printf("\nEnter the initial values of x, y, h, xn:");
+        scanf("%f %f %f %f",&x0, &y0, &h, &xn );
+        do
+        {
+                k1=h*f(x0, y0);
+                k2=h*f(x0+h/2, y0+k1/2);
+                k3=h*f(x0+h/2, y0+k2/2);
+                k4=h*f(x0+h, y0+k3);
+                k=(k1+2*k2+2*k3+k4)/6;
+                y0+=k;
+                x0+=h;
+        } while (x0<xn);
+        printf("\n for x=%.1f y=%.3f\n\n",xn,y0);
+}
