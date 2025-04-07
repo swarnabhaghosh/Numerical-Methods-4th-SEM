918edc3beca652ccf8d58f65d34157582e4093bd
Swarnabha Ghosh <swarnabha983@gmail.com>
2025-04-07 11:33:04 +0530

    commit

---
+++ Integration-Simpson's 1-3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x) (x*sin(x)+pow(x,3))
+#define x0 0
+
+/*
+h=(b-a)/n
+xi=x0+i*h, for (i = 1 to (n-1))
+yi=f(xi), for (i = 1 to (n-1))
+integration result= h/2[y0+yn+2*(sum of even terms)+4*(sum of remaining odd terms)] 
+*/
+
+void main()
+{
+        int i;
+        float a, b, aux1=0.0, aux2=0.0, n, sum, h, x; //aux1 = sum of even terms, aux2 = sum of remaining odd terms
+        printf("\nEnter a , b & n:"); //a=lower limit, b=upper limit, n= no. of subintervals
+        scanf("%f %f %f",&a,&b,&n);
+        h=(b-a)/n;
+        sum=f(a)+f(b); //sum of first and last term
+        for(i=0; i<n; i++) //sum of remaining terms
+        {
+                x=x0+i*h;
+                if(i%2==0) //if even
+                {
+                        aux1+=f(x);
+                        printf("\n x%d=%.4f,\ty%d=%.4f", i, x, i, f(x));
+                }
+                else //if odd
+                {
+                        aux2+=f(x);
+                        printf("\n x%d=%.4f,\ty%d=%.4f", i, x, i, f(x));
+                }
+        }
+        sum+=aux1*2+aux2*4;
+        sum*=h/3;
+        printf("\n Answer: %.3f",sum);
+}
--- Integration-Trapezoidal.c
+++ Integration-Trapezoidal.c
@@ -1,23 +1,30 @@
 #include<stdio.h>
 #include<math.h>
-#define f(x) (x*sin(x)+pow(x,3))
-#define x0 0
+#define f(x) (x*sin(x)+pow(x,3)) //given expression
+#define x0 0 //by default it will be 0
+
+/*
+h=(b-a)/n
+xi=x0+i*h, for (i = 1 to (n-1))
+yi=f(xi), for (i = 1 to (n-1))
+integration result= h/2[y0+yn+2*(y1+y2+...+y(n-1))] 
+*/
 
 void main()
 {
         int i;
-        float a, b, aux=0.0, n, sum, h, y;
-        printf("\nEnter a (lower limit), b (upper limit) & n:"); //a=lower limit, b=upper limit, n= no. of subintervals
+        float a, b, x=0.0, n, sum, h, y;
+        printf("\nEnter a, b & n:"); //a=lower limit, b=upper limit, n= no. of subintervals
         scanf("%f %f %f", &a, &b, &n);
         h=(b-a)/n; 
         sum=f(a)+f(b); //sum of first and the last term
-        for(i=0; i<n; i++)
+        for(i=1; i<n; i++) //for the remaining terms
         {
-                y=x0+i*h;
-                aux+=f(y);
-                printf("\n %d %.4f",i,y,f(y));
+                x=x0+i*h;
+                y+=f(x);
+                printf("\n x%d=%.4f,\ty%d=%.4f", i, x, i, f(x));
         }
-        sum+=aux*2;
+        sum+=y*2;
         sum*=h/2;
         printf("\n Answer: %.3f",sum);
 }
