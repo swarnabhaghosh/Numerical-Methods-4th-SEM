d7980881f4e3d55a00aa7021900de8e8ef707b2a
Swarnabha Ghosh <swarnabha983@gmail.com>
2025-05-05 01:18:12 +0530

    Update Newton-Raphson-Method.c

--- Newton-Raphson-Method.c
+++ Newton-Raphson-Method.c
@@ -1,56 +1,36 @@
 #include<stdio.h>
 #include<math.h>
-#define Error 0.001
-
-float f(float x) //implenting the equation
+float f(float x)
 {
-	return pow(x,3)-(3*x)-5 ;
+    return (pow(x,3)-3*x-5);
 }
-float d(float x) //differentiate the equation
+float d(float x)
 {
-	return (3*x*x)-3;
+    return (3*pow(x, 2)-3);
 }
-int main()
+void main()
 {
-	int mi,i;
-	float x1,x2,x,x0;
-	printf("Enter maximum number of iteration:");
-	scanf("%f",&mi);
+    float a, b, x0, x;
+    int i=1, mi;
+    printf("\nEnter a and b:");
+    scanf("%f %f", &a, &b);
 
-	do
-	{
-		printf("Enter the value of x1 and x2:"); //starting boundary
-		scanf("%f %f",&x1,&x2);
-		if(f(x1)*f(x2)>0)
-		{
-			printf("Boundary values are invalid");
-			continue;
-		}
-		else
-		{
-			printf("Roots are lie between %f and %f",x1,x2);
-			break;
-		}
-	}while(1);
+    printf("\nenter the maximum iteration:");
+    scanf("%d",&mi);
 
-	//finding x0
-	if(fabs(f(x1))<fabs(f(x2))) //taking the absolute value to check which is nearer to 0
-	{
-		x0=x1;
-	}
-	else
-	{
-		x0=x2;
-	}
-	//finding root
-	for(i=1;i<=mi;i++)
-	{
-		x=x0-(f(x0)/d(x0));
-		if(fabs(x-x0)<Error)
-			break;
-		printf("\nIteration:%d Roots:%f",i,x);
-		x0=x;
-	}
-	printf("\nRoot=%f Total Iteration=%d",x,--i);
-	return 0;
+    if(fabs(f(a))<fabs(f(b)))
+        x0=a;
+    else
+        x0=b;
+    
+    do
+    {
+        x=x0-f(x0)/d(x0);
+        if(fabs(x-x0)<0.001)
+            break;
+        printf("\niteration=%d, root=%f", i, x);
+        x0=x;
+        i++;
+    }while(i<=mi);
+    printf("\nresult=%.4f, total iteration",x0);
 }
